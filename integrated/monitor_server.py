d9cbe97e60c9f8527229c24f34381353ad7b2e73
changes made on development pi <from_server@danenet.org>
2015-01-29 00:51:02 -0600

    plot working a little - reworked inner loop pulling out alert tests

--- integrated/monitor_server.py
+++ integrated/monitor_server.py
@@ -16,41 +16,85 @@ from plot_temp_date import *
 device_folder = glob.glob('/sys/bus/w1/devices/28*')
 device_file = device_folder[0] + '/w1_slave'
 
-def act_on_reading():
-        
-    if os.path.exists('error_log.txt'): # if we have an active error
-        sendEmails = False              # don't send another email
+def check_alert(reading_in): #This is an iterator so variables are constant
+    
+    for dat in reading_in:    
+        if os.path.exists('error_log.txt'): # if we have an active error
+            sendEmails = False              # don't send another email
 
-    if (temp == -999):
-        if(sendEmails):
-            sendEmailSMTP('sensor error')
-
-    elif (temp < alertMinimum):
+        if (temp == -999):
             if(sendEmails):
-                sendEmailSMTP('low temperature alert')
+                sendEmailSMTP('sensor error')
 
-    elif (temp > alertMaximum):
-            if(sendEmails):
-                sendEmailSMTP('high temperature alert')
-    else:
-        print('T1:'+str(temp))
-        if(not sendEmails):        # since we got a valid answer delete the error log
-            os.remove('error_log.txt')
+        elif (temp < alertMinimum):
+                if(sendEmails):
+                    sendEmailSMTP('low temperature alert')
+
+        elif (temp > alertMaximum):
+                if(sendEmails):
+                    sendEmailSMTP('high temperature alert')
+        else:
+            print('T1:'+str(temp))
+            if(not sendEmails):        # since we got a valid answer delete the error log
+                os.remove('error_log.txt')
+
+                
+        yield dat  # this makes this function an iterator
 
 
+
+
+
+
+######################################
+# this is the top of the real program
+######################################
+
+device_folder = glob.glob('/sys/bus/w1/devices/28*')
+device_file = device_folder[0] + '/w1_slave'
 
 print "monitor_server: device_file",device_file
 
-# this is the top of the real program
-outfilename="readings"+time.strftime("%Y-%m-%dT%H-%M", time.localtime())
+
+outfilename="/var/www/readings"+time.strftime("%Y-%m-%d", time.localtime())
 OUTFILE = open(outfilename, "w")
 
+ONEDAY = 24*60*60
+PLOT_INT = 15*60
+last_housekeeping = time.time() - ONEDAY  #force first pass to trigger
+last_plot = time.time() - ONEDAY  #force first pass to trigger
+#print time.time(), type(time.time)
+#last_day = time.strftime("%Y-%m-%d", time.localtime()-ONEDAY)
+readings = []
+dates = []
+
+
 while 1:
-    temp = read_1wire_temp(device_file) #get the temp
-    output_str = "%s, %6.3f C, %6.3f F\n"%(time.strftime("%Y-%m-%dT%H:%M:%S", time.localtime()),
-                                          temp, (temp*1.800 + 32))
+    reading = read_1wire_temp(device_file) #get the temp
+    readings.append(reading)
+    dates.append(datetime.datetime.now())
+    
+    output_str = "%s, %6.3f\n"%(time.strftime("%Y-%m-%dT%H:%M:%S", time.localtime()),
+                                           (reading*1.800 + 32))
     print output_str
     OUTFILE.write(output_str)
     OUTFILE.flush()
+    check_alert(reading)
+
+    
+    today_str=time.strftime("%Y-%m-%d", time.localtime())
+    if (time.time() - last_plot) > PLOT_INT: 
+        plotdata(dates, readings, "/var/www/plot"+today_str)
+    
+    if (time.time() - last_housekeeping > ONEDAY): # and (today > last_day):
+        # daily tasks
+        readings=[]
+        dates = []
+        last_day = today_str
+        last_housekeeping = time.time()
+        outfilename="/var/www/readings"+time.strftime("%Y-%m-%d", time.localtime())
+        OUTFILE.close()
+        OUTFILE = open(outfilename, "w")
+    
     time.sleep(60*5)
 
--- integrated/plot_temp_date.py
+++ integrated/plot_temp_date.py
@@ -3,16 +3,16 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import matplotlib.image as mpimg
 
-def plotdata():
-  days, impressions = np.loadtxt("/home/pi/server_monitor/temps2015-01-01T10:09:44", unpack=True,
-          converters={ 0: mdates.strpdate2num('%Y-%m-%dT%H:%M:%S')})
-
-  plt.plot_date(x=days, y=impressions, fmt="r-")
+def plotdata(dates,readings, filename):
+  #days, temp_f = np.loadtxt("/home/pi/server_monitor/temps2015-01-01T10:09:44", unpack=True,
+  #        converters={ 0: mdates.strpdate2num('%Y-%m-%dT%H:%M:%S')})
+  
+  plt.plot_date(x=dates, y=readings, fmt="r-")
   plt.title("Time vs Temp")
   plt.ylabel("Temp (F)")
   plt.grid(True)
   #plt.show()
-  plt.savefig("out.png")
+  plt.savefig(filename)
 
 def main():
   plotdata()

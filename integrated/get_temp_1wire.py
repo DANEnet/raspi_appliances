c89563bb8c79726984ac9d498a7d4645cc930689
changes made on development pi <from_server@danenet.org>
2015-01-28 22:48:07 -0600

    before switching act_on_reading() to be an iterator

--- integrated/get_temp_1wire.py
+++ integrated/get_temp_1wire.py
@@ -1,18 +1,16 @@
 # this file is adapted from http://jartweb.net/blog/wp-content/uploads/2013/12/Raspberry-Pi-Logger-with-LCD.pdf
 
-import os, os.path, glob, time, sys, datetime
-
+import os, os.path
 maxTry = 1000       # number of tries before it gives up on reading
 
-def read_temp_raw(): # function that grabs the raw temperature data from the sensors
-    device_file = "/sys/bus/w1/devices/28-000005aa893d/w1_slave"
+def read_temp_raw(device_file): # function that grabs the raw temperature data from the sensors
     f = open(device_file, 'r') # the sensor appears as a file
     lines = f.readlines()      # this is a list of two lines
     f.close()
     return lines
 
-def read_2wire_temp(): # function that checks that the connection was good and pulls out the temperature
-    lines = read_temp_raw()
+def read_1wire_temp(device_file): # function that checks that the connection was good and pulls out the temperature
+    lines = read_temp_raw(device_file)
     numTry = 0
     while lines[0].strip()[-3:] != 'YES' and numTry < maxTry: # loop until we get a valid response
         time.sleep(0.5)
--- integrated/monitor_server.py
+++ integrated/monitor_server.py
@@ -9,44 +9,48 @@ alertMaximum = 35   # max temperature (C) before alerting
 alertMinimum = 5    # min temperature (C) before alerting
 maxTry = 1000       # number of tries before it gives up and sends an email
 
-from get_temp_2wire import *
+from get_temp_1wire import *
 from plot_temp_date import *
 # get the location of the DS18B20 in the system
 
 device_folder = glob.glob('/sys/bus/w1/devices/28*')
 device_file = device_folder[0] + '/w1_slave'
 
+def act_on_reading():
+        
+    if os.path.exists('error_log.txt'): # if we have an active error
+        sendEmails = False              # don't send another email
+
+    if (temp == -999):
+        if(sendEmails):
+            sendEmailSMTP('sensor error')
+
+    elif (temp < alertMinimum):
+            if(sendEmails):
+                sendEmailSMTP('low temperature alert')
+
+    elif (temp > alertMaximum):
+            if(sendEmails):
+                sendEmailSMTP('high temperature alert')
+    else:
+        print('T1:'+str(temp))
+        if(not sendEmails):        # since we got a valid answer delete the error log
+            os.remove('error_log.txt')
+
+
+
 print "monitor_server: device_file",device_file
 
 # this is the top of the real program
-outfilename="readings"+time.strftime("%Y-%m-%dT%H:%M:%S", time.localtime())
+outfilename="readings"+time.strftime("%Y-%m-%dT%H-%M", time.localtime())
 OUTFILE = open(outfilename, "w")
 
 while 1:
-    temp = read_2wire_temp() #get the temp
-    output_str = "%s, %f3.2 C, %f3.3 F\n"%(time.strftime("%Y-%m-%dT%H:%M:%S", time.localtime()),
+    temp = read_1wire_temp(device_file) #get the temp
+    output_str = "%s, %6.3f C, %6.3f F\n"%(time.strftime("%Y-%m-%dT%H:%M:%S", time.localtime()),
                                           temp, (temp*1.800 + 32))
     print output_str
     OUTFILE.write(output_str)
     OUTFILE.flush()
     time.sleep(60*5)
-
-if os.path.exists('error_log.txt'): # if we have an active error
-    sendEmails = False              # don't send another email
-
-if (temp == -999):
-    if(sendEmails):
-        sendEmailSMTP('sensor error')
-
-elif (temp < alertMinimum):
-        if(sendEmails):
-            sendEmailSMTP('low temperature alert')
-
-elif (temp > alertMaximum):
-        if(sendEmails):
-            sendEmailSMTP('high temperature alert')
-else:
-    print('T1:'+str(temp))
-    if(not sendEmails):        # since we got a valid answer delete the error log
-        os.remove('error_log.txt')
 

b9f06f2abc4b12e61a04c748ef46838d3ecd17a7
root <root@Temperature1.(none)>
2017-03-12 23:31:31 -0500

    add making pin 6 high to power the thermomiter

--- working/server_monitor.py
+++ working/server_monitor.py
@@ -14,7 +14,7 @@ from display_LCD import *
 
 import send_email 
 import get_config
-import  Adafruit_CharLCD as LCD
+import RPi.GPIO as GPIO
 
 
 
@@ -131,7 +131,7 @@ for pin in [6]:
 	GPIO.setup(pin, GPIO.OUT)
 	GPIO.output(pin, GPIO.HIGH)
 
-
+time.sleep(10)
 
 readings_f = []
 dates = []

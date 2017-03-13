eb8d299bd049ab5dfe3a375061a2f00936c527c5
root <root@Temperature1.(none)>
2017-03-12 23:57:04 -0500

    add try: to prevent a call to the LCD from crashing if there is no LCD

--- working/display_LCD.py
+++ working/display_LCD.py
@@ -2,7 +2,10 @@ import time
 import  Adafruit_CharLCD as LCD
 
 def display_LCD(time, tempc, tempf):
-  lcd = LCD.Adafruit_CharLCDPlate()
+  try:
+    lcd = LCD.Adafruit_CharLCDPlate()
+  except:
+    return
   lcd.set_color(1.0, 1.0, 1.0)
   lcd.clear()
   lcd.message('%5.1f F %5.1f C \n%s'%(tempf, tempc, time))
